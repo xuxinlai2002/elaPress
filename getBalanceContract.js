1224155b498da6eea5f6721fc67b4be74ad1ec3a
xxl <xxl@local.com>
2020-09-09 13:36:03 +0800

    add

--- getBalanceContract.js
+++ getBalanceContract.js
@@ -1,43 +1,58 @@
-let Web3 = require('web3');
-
-let web3Obj
-
-if (typeof web3Obj !== 'undefined') {
-    web3Obj = new Web3(web3.currentProvider);
-} else {
-    // set the provider you want from Web3.providers
-    web3Obj = new Web3(new Web3.providers.HttpProvider("http://0.0.0.0:20636"));
-    //web3Obj = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
-    //web3Obj = new Web3(new Web3.providers.WebsocketProvider("ws://127.0.0.1:8545"));
-}
+const Web3 = require("web3");
+const web3 = new Web3("http://127.0.0.1:20636");
+let acc = ""
 
 var arguments = process.argv.splice(2);
 
-let constractAddr = arguments[0];
-let balanceAddr = arguments[1];
+let constractAddr="0xEDA301E29EAe326F5DF125b8573f6a8Dd7865E29"//arguments[0];
+let toAddr="0xdb24e23e49cfc7c3cf15be02461387a060284964"//arguments[1];
 
 
-let bytecode = "0x608060405234801561001057600080fd5b5033600090815260208190526040902064174876e8009055610198806100376000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c806390b98a111461003b578063f8b2cb4f1461007b575b600080fd5b6100676004803603604081101561005157600080fd5b506001600160a01b0381351690602001356100b3565b604080519115158252519081900360200190f35b6100a16004803603602081101561009157600080fd5b50356001600160a01b0316610147565b60408051918252519081900360200190f35b336000908152602081905260408120548211156100d257506000610141565b33600081815260208181526040808320805487900390556001600160a01b03871680845292819020805487019055805193845290830191909152818101849052517fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef9181900360600190a15060015b92915050565b6001600160a01b03166000908152602081905260409020549056fea2646970667358221220114b46622bbba73729657daa51c703de4aaaaf9e36db1c21546f543b57ac9f6a64736f6c63430007010033";
-let abi = '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"_from","type":"address"},{"indexed":false,"internalType":"address","name":"_to","type":"address"},{"indexed":false,"internalType":"uint256","name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"addr","type":"address"}],"name":"getBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"internalType":"bool","name":"sufficient","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]'
+web3.eth.getAccounts().then(function(value){
+    acc=value[0];
+    console.log("===>", acc);
+
+    let bytecode = "0x608060405234801561001057600080fd5b5033600090815260208190526040902064174876e8009055610198806100376000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c806390b98a111461003b578063f8b2cb4f1461007b575b600080fd5b6100676004803603604081101561005157600080fd5b506001600160a01b0381351690602001356100b3565b604080519115158252519081900360200190f35b6100a16004803603602081101561009157600080fd5b50356001600160a01b0316610147565b60408051918252519081900360200190f35b336000908152602081905260408120548211156100d257506000610141565b33600081815260208181526040808320805487900390556001600160a01b03871680845292819020805487019055805193845290830191909152818101849052517fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef9181900360600190a15060015b92915050565b6001600160a01b03166000908152602081905260409020549056fea2646970667358221220114b46622bbba73729657daa51c703de4aaaaf9e36db1c21546f543b57ac9f6a64736f6c63430007010033";
+    let abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"_from","type":"address"},{"indexed":false,"internalType":"address","name":"_to","type":"address"},{"indexed":false,"internalType":"uint256","name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"addr","type":"address"}],"name":"getBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"internalType":"bool","name":"sufficient","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]
 
 
-// 相对于部署合约，多了第二个参数，即合约地址
-const myContract = new web3Obj.eth.Contract(JSON.parse(abi), constractAddr, {
-    // 非必填，合约的bytecode
-    data: bytecode,
-    // 非必填，合约的创建者
-    from: balanceAddr,
-    //Gas limit
-    gas: 4712388,
-    gasPrice: '1000000'
-});
+   //let contract = new web3.eth.Contract(abi);
+    let nonce = 0
+
+    //let contract = new web3.eth.Contract(abi);
+
+    // 相对于部署合约，多了第二个参数，即合约地址
+    const contract = new web3.eth.Contract(abi);
+    contract.options.address = constractAddr;
 
 
-// 调用合约中的getBalance方法
-myContract.methods.getBalance(balanceAddr).call({
-    //非必填，该合约方法的调用者
-    from: balanceAddr
-}, function (error, result) {
-    console.log('error:' + error)
-    console.log('result:' + result)
+    web3.eth.getTransactionCount(acc).then(function(num) {
+
+    nonce = num ; console.log("nonce:", nonce)    
+    //const data = contract.deploy({data: bytecode}).encodeABI();
+    //var data = contract.methods.sendCoin(toAddr,1).encodeABI();
+    var data = contract.methods.getBalance(acc).encodeABI();
+
+
+    // // 调用合约中的sendCoin方法
+    // myContract.methods.sendCoin(toAddr, 1).send({
+    //     //非必填，该合约方法的调用者
+    //     from: value[0]
+    // })
+
+
+    const tx = {data: data, from: acc, gas: "2000000", gasPrice: "3000000000",nonce:nonce, to:constractAddr};
+
+    web3.eth.signTransaction(tx).then((stx) => {
+    console.log("rawTx", stx.raw)
+
+        web3.eth.sendSignedTransaction(stx.raw).on("transactionHash", console.log).then(console.log).catch(console.log)
+
+
+    })
+
+   })
+
+    
 })
+
--- sendContract.js
+++ sendContract.js
@@ -4,8 +4,8 @@ let acc = ""
 
 var arguments = process.argv.splice(2);
 
-let constractAddr = arguments[0];
-let toAddr = arguments[1];
+let constractAddr="0xEDA301E29EAe326F5DF125b8573f6a8Dd7865E29"//arguments[0];
+let toAddr="0xdb24e23e49cfc7c3cf15be02461387a060284964"//arguments[1];
 
 
 web3.eth.getAccounts().then(function(value){
@@ -13,7 +13,7 @@ web3.eth.getAccounts().then(function(value){
     console.log("===>", acc);
 
     let bytecode = "0x608060405234801561001057600080fd5b5033600090815260208190526040902064174876e8009055610198806100376000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c806390b98a111461003b578063f8b2cb4f1461007b575b600080fd5b6100676004803603604081101561005157600080fd5b506001600160a01b0381351690602001356100b3565b604080519115158252519081900360200190f35b6100a16004803603602081101561009157600080fd5b50356001600160a01b0316610147565b60408051918252519081900360200190f35b336000908152602081905260408120548211156100d257506000610141565b33600081815260208181526040808320805487900390556001600160a01b03871680845292819020805487019055805193845290830191909152818101849052517fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef9181900360600190a15060015b92915050565b6001600160a01b03166000908152602081905260409020549056fea2646970667358221220114b46622bbba73729657daa51c703de4aaaaf9e36db1c21546f543b57ac9f6a64736f6c63430007010033";
-    let abi = '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"_from","type":"address"},{"indexed":false,"internalType":"address","name":"_to","type":"address"},{"indexed":false,"internalType":"uint256","name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"addr","type":"address"}],"name":"getBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"internalType":"bool","name":"sufficient","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]'
+    let abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"_from","type":"address"},{"indexed":false,"internalType":"address","name":"_to","type":"address"},{"indexed":false,"internalType":"uint256","name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"addr","type":"address"}],"name":"getBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"internalType":"bool","name":"sufficient","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]
 
 
    //let contract = new web3.eth.Contract(abi);
@@ -22,19 +22,16 @@ web3.eth.getAccounts().then(function(value){
     //let contract = new web3.eth.Contract(abi);
 
     // 相对于部署合约，多了第二个参数，即合约地址
-    const contract = new web3.eth.Contract(JSON.parse(abi), constractAddr, {
-        // 非必填，合约的bytecode
-        data: bytecode,
-        // 非必填，合约的创建者
-        from: value[0]
-    });
+    const contract = new web3.eth.Contract(abi);
+    contract.options.address = constractAddr;
 
 
     web3.eth.getTransactionCount(acc).then(function(num) {
 
-    nonce = num; console.log("nonce:", nonce)    
+    nonce = num ; console.log("nonce:", nonce)    
     //const data = contract.deploy({data: bytecode}).encodeABI();
     var data = contract.methods.sendCoin(toAddr,1).encodeABI();
+    //var data = contract.methods.getBalance(acc).encodeABI();
 
 
     // // 调用合约中的sendCoin方法
@@ -44,10 +41,10 @@ web3.eth.getAccounts().then(function(value){
     // })
 
 
-    const tx = {data: data, from: acc, gas: "2000000", gasPrice: "2000000000",nonce:nonce};
+    const tx = {data: data, from: acc, gas: "2000000", gasPrice: "3000000000",nonce:nonce, to:constractAddr};
 
     web3.eth.signTransaction(tx).then((stx) => {
-    console.log("mmm", stx.raw)
+    console.log("rawTx", stx.raw)
 
         web3.eth.sendSignedTransaction(stx.raw).on("transactionHash", console.log).then(console.log).catch(console.log)
 
