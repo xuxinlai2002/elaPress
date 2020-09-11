66c2f606ab4373741b681d192d9dc87c5702f332
xxl <xxl@local.com>
2020-09-11 09:30:37 +0800

    add

--- runSendContract.sh
+++ runSendContract.sh
@@ -6,7 +6,7 @@ while true
 do
     count=$((${count} + 1))
     echo "start :`date '+%Y%m%d %H:%M:%S'` ...$count"
-    node sendContract-web3-interface.js 0xE0B33898893FB47283f953b0D4dfcD687e69137c 0xcd0c4191a2106c134a87021854baa4d35c01f7ba
+    node sendContract-web3-interface.js 0xaff851606CB8E24A5a559d9942b17fc59AFc3090 0xcd0c4191a2106c134a87021854baa4d35c01f7ba
     echo "end   :`date '+%Y%m%d %H:%M:%S'` ...$count"
     echo ""
 
