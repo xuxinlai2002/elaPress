4d5e535afacef5609f2f1bc1c7d77622019d88dc
xxl <xxl@local.com>
2020-09-14 07:21:20 +0800

    add

--- withdraw.js
+++ withdraw.js
@@ -2,8 +2,8 @@ Web3 = require("web3");
 web3 = new Web3("http://127.0.0.1:20636");
 
 amount_wei=web3.utils.toWei("0.01")
-from_ks=""
-withdraw_to=""
+from_ks="0x53781E106a2e3378083bdcEdE1874E5c2a7225f8"
+withdraw_to="0x41eA6aD88bbf4E22686386783e7817bB7E82c1ed"
 fee_wei=web3.utils.toWei("0.00010001")
 
 contract = new web3.eth.Contract([{"constant":false,"inputs":[{"name":"_addr","type":"string"},{"name":"_amount","type":"uint256"},{"name":"_fee","type":"uint256"}],"name":"receivePayload","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_addr","type":"string"},{"indexed":false,"name":"_amount","type":"uint256"},{"indexed":false,"name":"_crosschainamount","type":"uint256"},{"indexed":true,"name":"_sender","type":"address"}],"name":"PayloadReceived","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_sender","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"},{"indexed":true,"name":"_black","type":"address"}],"name":"EtherDeposited","type":"event"}]);
