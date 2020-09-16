be718be6e25ea4d13cf399995fc2fef187a97d28
xxl <xxl@local.com>
2020-09-16 13:56:10 +0800

    add

--- elaToEth.sh
+++ elaToEth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+cd ~/ela/ela_node_regtest/account_xxl
 
 ../ela-cli wallet buildtx crosschain --saddress "XFNwHEkHvAvMK7b3BuF5dJUJ1ZvbpprQ14" --to "0x86c354d9e61169fe6fc8a14820dba0241f1f1f2d" --amount "1" --fee "0.0001" --rpcport 22336 --rpcuser User --rpcpassword Password 
 
