ecd63a296fcb2d2132a02233f8826167970a94eb
xxl <xxl@local.com>
2020-09-21 08:33:11 +0800

    add

--- runElaToEth.sh
+++ runElaToEth.sh
@@ -8,7 +8,8 @@ while true
 do
     count=$((${count} + 1))
     echo "start :`date '+%Y%m%d %H:%M:%S'` ...$count"
-    python3 /home/dev/testframework/Elastos.ELA.TestFramework/pressure_xxl_01.py $1 > ~/elaPress/pressure_xxl_01.log
+    cd /home/dev/testframework/Elastos.ELA.TestFramework/
+    python3 ./pressure_xxl_01.py $1 > ~/elaPress/pressure_xxl_01.log
     echo "end   :`date '+%Y%m%d %H:%M:%S'` ...$count"
     echo ""
 
